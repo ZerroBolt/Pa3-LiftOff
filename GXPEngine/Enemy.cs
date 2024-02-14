097a25f53c7e5f0d17aee8130de5f544fbe33bfc
Tycho <tychokerkhofs@gmail.com>
2024-02-14 13:04:46 +0100

    - Made basic functionality for enemy - Changed gitignore file

---
+++ GXPEngine/Enemy.cs
@@ -0,0 +1,117 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiledMapParser;
+
+class Enemy : AnimationSprite
+{
+    const string imageLocation = "barry.png";
+
+    float speed = 0.5f;
+    int health = 1;
+
+    Sprite target;
+
+    int _damage = 1;
+    public int damage
+    {
+        get { return _damage; }
+        set { _damage = value; }
+    }
+
+    public Enemy(TiledObject obj=null) : base(imageLocation, 7, 1)
+    {
+        Initialize(obj);
+    }
+
+    private void Initialize(TiledObject obj)
+    {
+        SetOrigin(width / 2, height / 2);
+        SetCycle(0, 7);
+        collider.isTrigger = true;
+
+        if (obj != null)
+        {
+            speed = obj.GetFloatProperty("speed", 1f);
+            health = obj.GetIntProperty("health", 1);
+            damage = obj.GetIntProperty("damage", 1);
+        }
+
+        setRandomPosition();
+    }
+
+    private void setRandomPosition()
+    {
+        //switch (Utils.Random(0, 2))
+        //{
+        //    case 0:
+        //        x = Utils.Random(game.width, game.width + width);
+        //        break;
+        //    case 1:
+        //        x = Utils.Random(0 - width, 0);
+        //        break;
+        //}
+
+        //switch (Utils.Random(0, 2))
+        //{
+        //    case 0:
+        //        y = Utils.Random(game.height, game.height + height);
+        //        break;
+        //    case 1:
+        //        y = Utils.Random(0 - height, 0);
+        //        break;
+        //}
+
+        x = Utils.Random(0 - this.width, game.width + this.width);
+        y = Utils.Random(0 - this.height, game.height + this.height);
+    }
+
+    public void SetTarget(Sprite target)
+    {
+        this.target = target;
+    }
+
+    private void Update()
+    {
+        float oldX = x;
+        //TODO: move from outside of screen to center of screen
+        if (x < target.x)
+        {
+            x += speed;
+        }
+        else
+        {
+            x -= speed;
+        }
+
+        if (x < oldX)
+        {
+            Mirror(true, false);
+        }
+        else
+        {
+            Mirror(false, false);
+        }
+
+        if (y < target.y)
+        {
+            y += speed;
+        }
+        else
+        {
+            y -= speed;
+        }
+
+        //TODO: Check for collision
+        //Do damage when it is the house
+        if (HitTest(target))
+        {
+            this.Destroy();
+        }
+
+        Animate(0.25f);
+    }
+}
--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -1,23 +1,39 @@
 using System;                                   // System contains a lot of default C# libraries 
 using GXPEngine;                                // GXPEngine contains the engine
-using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
+using System.Drawing;
+using System.Collections.Generic;                           // System.Drawing contains drawing tools such as Color definitions
 
 public class MyGame : Game {
-	public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
+	Sprite sp;
+    List<Enemy> enemyList = new List<Enemy>();
+    public MyGame() : base(1366, 768, false)     // Arcade screen is 1366 x 768 pixels
 	{
-		// Draw some things on a canvas:
-		EasyDraw canvas = new EasyDraw(800, 600);
-		canvas.Clear(Color.MediumPurple);
-		canvas.Fill(Color.Yellow);
-		canvas.Ellipse(width / 2, height / 2, 200, 200);
-		canvas.Fill(50);
-		canvas.TextSize(32);
-		canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-		canvas.Text("Welcome!", width / 2, height / 2);
+		//// Draw some things on a canvas:
+		//EasyDraw canvas = new EasyDraw(800, 600);
+		//canvas.Clear(Color.MediumPurple);
+		//canvas.Fill(Color.Yellow);
+		//canvas.Ellipse(width / 2, height / 2, 200, 200);
+		//canvas.Fill(50);
+		//canvas.TextSize(32);
+		//canvas.TextAlign(CenterMode.Center, CenterMode.Center);
+		//canvas.Text("Welcome!", width / 2, height / 2);
 
-		// Add the canvas to the engine to display it:
-		AddChild(canvas);
-		Console.WriteLine("MyGame initialized");
+		//// Add the canvas to the engine to display it:
+		//AddChild(canvas);
+		//Console.WriteLine("MyGame initialized");
+
+		sp = new Sprite("square.png");
+		sp.SetOrigin(sp.width / 2, sp.height / 2);
+		sp.SetXY(width / 2, height / 2);
+		AddChild(sp);
+
+		for (int i = 0; i < 10; i++)
+		{
+			Enemy enemy = new Enemy();
+            enemy.SetTarget(sp);
+            enemyList.Add(enemy);
+            AddChild(enemy);
+        }
 	}
 
 	// For every game object, Update is called every frame, by the engine:
